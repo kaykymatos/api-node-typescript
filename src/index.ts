9f56d319e2ac0d9e3f65f9de7cc4f0751fc51f64
Kayky Matos <kayky7277@gmail.com>
2023-07-21 20:47:55 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { server } from "./server/appserver";
+
+server.listen(3333,()=>{console.log("Teste rodando")})
---
+++ src/server/appserver.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const server = express();
+
+server.get("/",(_,res)=>{
+    return res.send("Hello")
+})
+export {server}
