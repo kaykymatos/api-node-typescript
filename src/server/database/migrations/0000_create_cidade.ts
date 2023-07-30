a0aee60c30629f2c2be8ebdedc2db26b84d6c787
Kayky Matos <kayky7277@gmail.com>
2023-07-30 11:26:01 -0300

    [8-knex-config] knex initial migration created

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,20 @@
+import { Knex } from 'knex';
+import { ETableNames } from '../ETableNames';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema
+    .createTable(ETableNames.cidade, (table) => {
+      table.bigIncrements('id').primary().index();
+      table.string('nome', 150).index().notNullable();
+      table.comment('Tabela usada para armazenar as cidades do sistema');
+    })
+    .then(() => {
+      console.log(`# Create table ${ETableNames.cidade}`);
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(ETableNames.cidade).then(() => {
+    console.log(`# Drop table ${ETableNames.cidade}`);
+  });
+}
