1c4aa5b1a589f5cc06ad3ae2f5ba5e2607d98628
Kayky Matos <kayky7277@gmail.com>
2023-08-01 19:57:44 -0300

    [8-knex-config] first seed created

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+import knex from 'knex';
 import { server } from './server/appserver';
+import { Knex } from './server/database/knex';
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(
-    `App rodando porta ${process.env.PORT || 3333} \nurl:http://localhost:${
-      process.env.PORT || 3333
-    }`
-  );
-});
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(
+      `App rodando porta ${process.env.PORT || 3333} \nurl:http://localhost:${
+        process.env.PORT || 3333
+      }`
+    );
+  });
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      Knex.seed
+        .run()
+        .then(() => {
+          startServer();
+        })
+        .catch(console.log);
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
