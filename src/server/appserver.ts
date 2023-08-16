7ad8d3b66418fad5ad75654fe10cdf218aaa2fd5
Kayky Matos <kayky7277@gmail.com>
2023-08-16 20:55:43 -0300

    [20-database-config] cors config

--- src/server/appserver.ts
+++ src/server/appserver.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import './shared/services/translationsYup';
 import { router } from './routes';
 import 'dotenv/config';
+import cors from 'cors';
 
 const server = express();
 
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(';')||[],
+  })
+);
 server.use(express.json());
 server.use(router);
 
